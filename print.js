67ca8861dc307d827e1278bc060fc4b89d1c406c
Ishan Singh <thakuri.thakuri2015@gmail.com>
2025-01-09 21:45:29 +0530

    day 2 and 3

---
+++ print.js
@@ -0,0 +1,29 @@
+//Primitive Data Types
+a="SECE"
+console.log(a)
+b = true
+console.log(b)
+//checking the type of the variable
+console.log(typeof(a))
+
+//secondary data types(object)
+array=['a',10,true,[20,30],"Sri Eshwar"]
+console.log(array)
+console.log(typeof(array))
+
+//Object
+obj={
+    firstname:"Sri",
+    lastname:"Eshwar",
+}
+console.log(obj)
+console.log(typeof(obj))
+console.log(obj.firstname)
+console.log(obj["lastname"])
+
+//second way of declaring object
+obj1={}
+console.log(obj1)
+obj1["dept"]="CSE"
+obj1['sec']="A"
+console.log(obj1)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+obj2 = new Object()
+console.log(obj2);
+obj2.count=70;
+obj2.abs=1
+totalPresentCount=obj2.count-obj2.abs
+console.log("Present",totalPresentCount)
+console.log(obj2)
