0c170cb56d0a8939eeaf7c53a5639f0a8670021e
Ishan Singh <thakuri.thakuri2015@gmail.com>
2025-01-20 12:59:36 +0530

    component and classComponent

---
+++ react/MERNStack/src/classcomponents/ClassCompEG.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class ClassCompEG extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Class Component Example</h1>
+            </div>
+        )
+    }
+}
+export default ClassCompEG;
+
+ 
